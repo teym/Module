179f2687e9d4c2887ec131dabf6fcc779df6f7be
Mike <ccteym@gmail.com>
2017-09-21 10:27:52 +0800

    init

---
+++ Module/Module.h
@@ -0,0 +1,19 @@
+//
+//  Module.h
+//  Module
+//
+//  Created by mike on 2017/9/20.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Module.
+FOUNDATION_EXPORT double ModuleVersionNumber;
+
+//! Project version string for Module.
+FOUNDATION_EXPORT const unsigned char ModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Module/PublicHeader.h>
+
+
